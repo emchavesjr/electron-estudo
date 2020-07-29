871be4e4d4586ffde4334c67ef511ba8de660076
Emanoel <emchavesjr@gmail.com>
2020-07-29 13:59:15 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require('electron')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
